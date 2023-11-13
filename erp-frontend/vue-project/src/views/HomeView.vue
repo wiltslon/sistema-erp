e4c159f7af863a8ba4e01faa81b3ceb514f2a430
wiltslon <wiltslon@gmail.com>
2023-11-13 12:24:32 -0300

    Edited home

--- erp-frontend/vue-project/src/views/HomeView.vue
+++ erp-frontend/vue-project/src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
-  <main>
-    <h1>Welcome vue programing</h1>
-  </main>
+<div class="conteinar">
+  <div class="card mt-5 m-lg-5">
+    <div class="card-header">
+      <h4>Wiltslon da Silva Pereira</h4>
+    </div>
+    <div class="card-body">
+    <span>Sistema criado como um desafio utilizando os frameworks VUE.JS 3 e Laravel 10.</span>
+
+    </div>
+  </div>
+</div>
 </template>
